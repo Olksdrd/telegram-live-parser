e36176e35f6fe70764f4c88a36f0410fe6e4ec2e
Oleksandr Drozdov <alxder.eeb@gmail.com>
2024-09-01 19:22:30 +0300

    Add logging to parser script

--- channel_parser.py
+++ channel_parser.py
@@ -1,3 +1,4 @@
+import logging
 import json
 
 from telethon import TelegramClient, events
@@ -8,36 +9,50 @@ from utils.chats_helpers import get_chat_name, get_event_id
 
 def start(keys, chats):
 
+    logging.info('Connecting to database...')
     _, collection = connect_to_mongo()
+    logging.info('Connection established.')
 
+    logging.info('Initializing Telegram Client...')
     tg_client = TelegramClient(
         keys['session_name'],
         keys['api_id'],
         keys['api_hash'])
     tg_client.start()
+    logging.info('Telegram Client started.')
 
-    print(f'Parsing data from {len(chats)} chats.')
+    logging.info(f'Parsing data from {len(chats)} chats...')
 
     @tg_client.on(events.NewMessage(chats=list(chats.values())))
     async def handler(event):
-        # print(event)
-        if event.message.message != '':
+
+        if event.message.message != '':  # parse only messages with text
             chat_id = get_event_id(event)
-            print(event.message.id, chat_id)
+            chat_name = get_chat_name(chat_id)
+
             document = {
                 'Message': event.message.message,
                 'Date': event.message.date,
-                'Chat_Name': get_chat_name(chat_id),
+                'Chat_Name': chat_name,
                 'Message_ID': event.message.id,
-                }
+            }
 
             response = collection.insert_one(document)
-            print(response)
+            logging.info(
+                f'Added message {event.message.id} '
+                f'from chat "{chat_name}". '
+                f'Record ID: {response.inserted_id}.'
+            )
 
     tg_client.run_until_disconnected()
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(levelname)s]: %(message)s'
+    )
+
     with open('tg-keys.json', 'r') as f:
         keys = json.load(f)
 
