fa589502182b8657b7bc12b8890816040efa94d1
Oleksandr Drozdov <alxder.eeb@gmail.com>
2024-09-06 19:00:10 +0300

    Add dataclass for short version of a message
    - NOTE: this commit will break the dashboard, but it will
    be fixed before merging into main
    - API response is too large, so it's useful to have some
    intermediate step between it and an object that we put
    in the database. Aslo, it's easier to work with it for
    some other operations in the future.

--- src/live_parser.py
+++ src/live_parser.py
@@ -4,14 +4,16 @@ from pathlib import Path
 import logging
 import json
 import asyncio
+from dataclasses import asdict
 
 from dotenv import load_dotenv
 
-from telethon import TelegramClient, events
+from telethon import TelegramClient
+from telethon.events import NewMessage
 
 sys.path.insert(0, os.getcwd())
 import utils.db_helpers as db  # noqa: E402
-import utils.parser_helpers as parser  # noqa: E402
+from utils.parser_helpers import CompactMessage  # noqa: E402
 
 
 def configure() -> tuple[dict[str, int], list[int]]:
@@ -33,7 +35,7 @@ def configure() -> tuple[dict[str, int], list[int]]:
     return keys, chat_ids
 
 
-async def start(
+async def live_parser(
     keys: dict[str, int],
     chat_ids: list[int],
     collection: db.Collection
@@ -44,40 +46,28 @@ async def start(
         keys['session_name'],
         keys['api_id'],
         keys['api_hash'],
-        catch_up=True  # does catchup even work?
+        catch_up=True
     )
     await tg_client.start()
     logging.info('Telegram Client started.')
 
     logging.info(f'Parsing data from {len(chat_ids)} chats...')
 
-    @tg_client.on(events.NewMessage(
+    @tg_client.on(NewMessage(
         chats=chat_ids,
         # commented out so that both incoming and outgoing messages are parsed
         # useful for testing: just send some message to yourself
         # incoming=True
         ))
-    async def handler(event):
+    async def handler(event: NewMessage.Event) -> None:
         # parse only messages with text, though images may also be of interest
         if event.message.message != '':  # tbh messages with len 1 are useless
-            chat_id = parser.get_dialog_id(event)
-            chat_name = parser.get_chat_name(chat_id)
+            document = CompactMessage.build_from_event(event)
 
-            document = {
-                'Message': event.message.message,
-                # mongoDB converts time back to UTC :(
-                # ! passing str here will break dashboard
-                'Date': parser.change_timezone(event.message.date),
-                'Chat_Name': chat_name,
-                'Message_ID': event.message.id,
-                # TODO: investigate other attributes in more details
-            }
-            # note that chat_id + Message_ID should provide unique primary key
-
-            response = collection.insert_one(document)
+            response = collection.insert_one(asdict(document))
             logging.info(
-                f'Added message {event.message.id} '
-                f'from chat "{chat_name}". '
+                f'Added message {document.msg_id} '
+                f'from chat "{document.chat_name}". '
                 f'Record ID: {response.inserted_id}.'
             )
 
@@ -95,7 +85,7 @@ def main() -> None:
 
     # handle SIGINT without an error message from asyncio
     try:
-        asyncio.run(start(keys, chats, collection))
+        asyncio.run(live_parser(keys, chats, collection))
     except KeyboardInterrupt:
         db_client.close()
         pass  # TelegramClient connection autocloses on SIGINT
--- utils/parser_helpers.py
+++ utils/parser_helpers.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 from datetime import datetime
+from dataclasses import dataclass
+from typing import Self
 import json
 import pytz
 
@@ -62,3 +64,30 @@ def change_timezone(
 
 def get_dialog_id(event: NewMessage.Event) -> int:
     return resolve_id(event.message.chat_id)[0]
+
+
+@dataclass(frozen=True)
+class CompactMessage:
+    """
+    More compact representation of a message.
+    Only stores the attributes we actually need.
+    """
+    msg_id: int
+    chat_id: int
+    chat_name: str
+    msg: str
+    date: datetime
+    # TODO: investigate other attributes in more details
+    # forward: Optional[Chat] or just id
+    # note that chat_id + msg_id should provide unique primary key
+
+    @classmethod
+    def build_from_event(cls: Self, event: NewMessage.Event) -> Self:
+        chat_id = get_dialog_id(event)
+        return CompactMessage(
+            msg_id=event.message.id,
+            chat_id=chat_id,
+            chat_name=get_chat_name(chat_id),
+            msg=event.message.message,
+            date=event.message.date
+        )
