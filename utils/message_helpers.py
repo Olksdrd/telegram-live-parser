3dc8452fbb49248d911d09539787ba401a3e3db6
Oleksandr Drozdov <alxder.eeb@gmail.com>
2024-09-28 14:31:40 +0300

    Move forward info extraction logic into a separate function
    - track .from_name forwards too
    - also some minor type hints changes

--- utils/message_helpers.py
+++ utils/message_helpers.py
@@ -3,12 +3,18 @@ import os
 import sys
 from datetime import datetime
 from pathlib import Path
-from typing import Callable, Coroutine, Self, TypedDict
+from typing import Any, Callable, Self, TypedDict
 
 from dotenv import load_dotenv
 from telethon import TelegramClient, functions
 from telethon.tl.custom.message import Message
-from telethon.tl.types import Document, MessageReactions, MessageReplies, ReactionCount
+from telethon.tl.types import (
+    Document,
+    MessageReactions,
+    MessageReplies,
+    ReactionCount,
+    MessageFwdHeader,
+)
 from telethon.utils import resolve_id
 
 sys.path.insert(0, os.getcwd())
@@ -46,7 +52,7 @@ def extract_custom_emoji_alt(document: Document) -> str:
             return attribute.alt
 
 
-def cache_custom_emoji_requests() -> Callable:
+def cache_custom_emoji_requests() -> Callable[[TelegramClient, int], str]:
     """
     Since requests are slow and channels usually use only a couple of custom emojis,
     which are repeated in every message, caching drastically reduces the number of
@@ -55,7 +61,7 @@ def cache_custom_emoji_requests() -> Callable:
     functools.lru_cache is not suitable, since we don't want to cache a client too,
     and cachetools is not in standard library -> easier to just use a dictionary.
     """
-    cache = {}
+    cache: dict[int, str] = {}
 
     async def get_custom_emoji_alt(client: TelegramClient, document_id: int) -> str:
         alt = cache.get(document_id)
@@ -92,7 +98,7 @@ async def unwrap_reactions(
     Ex: {'😁': 23, '👍': 12, '❤': 2}
     Note that it's pointless to use it on recent messages
     """
-    reactions = {}
+    reactions: dict[str, int] = {}
     if msg_reactions is None:
         return reactions
     for reaction_obj in msg_reactions.results:
@@ -107,8 +113,19 @@ async def unwrap_reactions(
 def get_reply_count(replies_obj: MessageReplies | None) -> int:
     if replies_obj is None:
         return 0
+    return replies_obj.replies
+
+
+async def get_fwd_from_info(
+    client: TelegramClient, from_peer: MessageFwdHeader
+) -> dict[str, Any]:
+    if from_peer.from_id is not None:
+        fwd_peer_info = await query_entity_info(client, from_peer.from_id)
+    elif from_peer.from_name is not None:
+        fwd_peer_info = {"full_name": from_peer.from_name}
     else:
-        return replies_obj.replies
+        fwd_peer_info = {}
+    return fwd_peer_info
 
 
 class CompactMessage(TypedDict, total=False):
@@ -117,18 +134,17 @@ class CompactMessage(TypedDict, total=False):
     Only stores the attributes we actually need.
     """
 
-    msg_id: int
-    chat_id: int
     msg: str
     date: datetime
+    msg_id: int
+    chat_id: int
     chat_name: str
     chat_title: str
     views: int
     forwards: int
     replies: int
     reactions: dict[str, int]
-    fwd_from: int
-    # TODO: investigate other attributes in more details
+    fwd_from: dict[str, Any]
     # note that chat_id + msg_id should provide unique primary key
 
 
@@ -151,12 +167,13 @@ class MessageBuilder:
         self._msg = CompactMessage()
 
     async def extract_text(self, new_msg: Message) -> Self:
-        self._msg["msg_id"] = new_msg.id
         self._msg["msg"] = new_msg.message
         self._msg["date"] = new_msg.date
         return self
 
-    async def extract_dialog_info(self, new_msg: Message) -> Coroutine:
+    async def extract_dialog_info(self, new_msg: Message) -> Self:
+        self._msg["msg_id"] = new_msg.id
+
         dialog_id = get_dialog_id(new_msg)
         self._msg["chat_id"] = dialog_id
 
@@ -167,7 +184,7 @@ class MessageBuilder:
         self._msg["chat_title"] = chat_title
         return self
 
-    async def extract_engagements(self, new_msg: Message) -> Coroutine:
+    async def extract_engagements(self, new_msg: Message) -> Self:
         self._msg["views"] = new_msg.views
         self._msg["forwards"] = new_msg.forwards
         self._msg["replies"] = get_reply_count(new_msg.replies)
@@ -176,17 +193,13 @@ class MessageBuilder:
         )
         return self
 
-    async def extract_forward_info(self, new_msg: Message) -> Coroutine:
+    async def extract_forward_info(self, new_msg: Message) -> Self:
         forwarded_from_peer = new_msg.fwd_from
         if forwarded_from_peer is not None:
-            peer = forwarded_from_peer.from_id
-            if peer is not None:
-                compact_dialog = await query_entity_info(self.client, peer)
-                self._msg["fwd_from"] = {
-                    str(key): val
-                    for key, val in compact_dialog.items()
-                    if val is not None
-                }
+            fwd_peer_info = await get_fwd_from_info(self.client, forwarded_from_peer)
+            self._msg["fwd_from"] = {
+                str(key): val for key, val in fwd_peer_info.items() if val is not None
+            }
         return self
 
     def build(self) -> CompactMessage:
